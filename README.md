b7ee17d1c55250bba156c494725324c21edc6fe6
RamGoel <rgoel766@gmail.com>
2022-04-06 00:53:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-xqxhks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-xqxhks)
---
+++ index.js
@@ -0,0 +1,11 @@
+
+function handleClick() {
+  const value = document.getElementById('codeArea').value;
+  const arr=value.split(' ');
+  console.log(arr)
+}
+
+
+
+
+
